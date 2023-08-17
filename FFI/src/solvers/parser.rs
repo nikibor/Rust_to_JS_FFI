081d2c3c02cfbb710da50aabdbf95a29ee6ef634
Nikita Borgolov <nborgolov96@gmail.com>
2023-08-17 15:27:28 +0200

    Add config structure and example file

--- FFI/src/solvers/parser.rs
+++ FFI/src/solvers/parser.rs
@@ -1,7 +1,25 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Config {}
+pub enum ConfigKind{
+    Congig,
+    Rule,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Cluster{
+    proxy_url: String,
+    server: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    api_version: String,
+    kind: ConfigKind,
+    clusters: Vec<Cluster>,
+    name: String,
+    users: Vec<String>
+}
 
 pub struct ConfigParser {
     path_dir: String,
